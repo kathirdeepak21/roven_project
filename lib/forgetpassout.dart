5b5e806dab6e705efeeabccadc05044c975c1cc9
kathirdeepak21 <150175154+kathirdeepak21@users.noreply.github.com>
2025-03-24 20:39:00 +0530

    1

---
+++ lib/forgetpassout.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Forgetpassout extends StatelessWidget {
+  const Forgetpassout({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          leading: Image.asset(
+        "images/jitlogo-removedbg.png",
+        height: 50,
+        width: 50,
+      )),
+    );
+  }
+}
